68d0be7b8702bfe1cebe4f4b3f9f5f7950305db5
jhgcgc <155690754+jhgcgc@users.noreply.github.com>
2024-01-25 21:07:26 -0300

    subir meu site

---
+++ script.js
@@ -0,0 +1,29 @@
+
+function insert(num){
+ const numero=document.getElementById("res").innerHTML;
+document.getElementById("res").innerHTML=numero+num
+}
+function clean(){ 
+    document.getElementById("res").innerHTML=""
+
+
+}
+function back(){
+const resultado=document.getElementById("res").innerHTML;
+document.getElementById("res").innerHTML=resultado.substring(0,resultado.length-1)
+
+
+}
+function calcular(){
+   var resultado=document.getElementById("res").innerHTML
+   if(resultado){
+    document.getElementById("res").innerHTML=eval(resultado);
+
+   }
+
+
+
+}
+
+
+
